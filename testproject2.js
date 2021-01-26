2b3ce541a40904139625ebb092029fb676cfe50d
Phukan <P10493903@capita.co.uk>
2021-01-26 11:57:22 +0000

    second test

--- testproject2.js
+++ testproject2.js
@@ -6,10 +6,7 @@ const webdriver = require("selenium-webdriver");
   try {
     await driver.get("https://aw1.automationintesting.online/");
     await driver.findElement( webdriver.By.className("btn btn-outline-primary float-right openBooking")).click();
-    await driver.findElement(webdriver.By.name("firstname")).sendKeys("Meghali");
-    await driver.findElement(webdriver.By.name("lastname")).sendKeys("Phukan");
-    await driver.findElement(webdriver.By.name("email")).sendKeys("meghaliphukan@gmail.com");
-    await driver.findElement(webdriver.By.name("phone")).sendKeys("782793298899");
+    
     // Click on Next button of Calendar
     await driver.findElement(webdriver.By.xpath('//*[@id="root"]/div[2]/div/div[4]/div/div[2]/div[2]/div/div[1]/span[1]/button[3]')).click();
     let src = driver.findElement(webdriver.By.className("rbc-date-cell rbc-current"));
@@ -17,6 +14,11 @@ const webdriver = require("selenium-webdriver");
     const actions = driver.actions({ async: true });
     // Performs drag and drop action of source element onto the target element
     await actions.dragAndDrop(src, target).perform();
+    //Fill the customer details and click on book
+    await driver.findElement(webdriver.By.name("firstname")).sendKeys("Meghali");
+    await driver.findElement(webdriver.By.name("lastname")).sendKeys("Phukan");
+    await driver.findElement(webdriver.By.name("email")).sendKeys("meghaliphukan@gmail.com");
+    await driver.findElement(webdriver.By.name("phone")).sendKeys("782793298899");
     await driver.findElement(webdriver.By.className("btn btn-outline-primary float-right book-room")).click();
   } catch (err) {
     console.log(err);
